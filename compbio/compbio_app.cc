709b2fb404e6e6505e67b0acd4fca0a7707a9565
Alexander Bikadorov <abiku@cs.tu-berlin.de>
2013-12-12 17:28:45 +0100

    added default compbio app with changed cycles (as stated in the assignment)

---
+++ compbio/compbio_app.cc
@@ -0,0 +1,54 @@
+#include <core/types.hh>
+#include <core/init.hh>
+#include <utility/exit.hh>
+#include <utility/excn/EXCN_Base.hh>
+#include <utility/string_util.hh>
+#include <basic/Tracer.hh>
+// option key includes
+#include <basic/options/option.hh>
+#include <basic/options/after_opts.hh>
+#include <basic/options/keys/run.OptionKeys.gen.hh>
+#include <basic/options/keys/out.OptionKeys.gen.hh>
+#include <utility/excn/Exceptions.hh>
+
+//# RBO_abinitio code
+#include <sys/stat.h>
+#include <apps/pilot/compbio/compbio_abinitio.hh>
+
+static basic::Tracer tr( "apps.pilot.compbio" );
+
+int
+main( int argc, char * argv [] )
+{
+    using namespace basic::options;
+    using namespace basic::options::OptionKeys;
+    compbio_abinitio::register_options();
+    core::init( argc, argv );
+
+    struct stat st;
+    if ( stat( "./models", &st ) == 0 )
+    {
+        system( "rm -vr models" );
+    }
+
+    if ( option[out::nstruct] && option[out::nstruct] > 0 )
+    {
+        tr.Info << "nstruct option: " << option[out::nstruct] << std::endl;
+        int num_decoys = option[out::nstruct];
+        for ( int i = 0; i < num_decoys+1; i++ )
+        {
+            compbio_abinitio ab( i );
+            ab.run();
+        }
+    }
+    else
+    {
+        compbio_abinitio ab( 0 );
+        ab.run();
+    }
+    system( "mkdir -v models" );
+    system( "mv -v *.pdb models" );
+    system( "mv -v score.fsc models" );
+
+    return 0;
+}
