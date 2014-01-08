c7318aaa741789a4117be3e3107fa17be9ffb590
Alexander Bikadorov <abiku@cs.tu-berlin.de>
2014-01-08 19:58:59 +0100

    extended script: the list of atom pairs from TMalign is parsed. Generating the rosetta constraint file is still missing.

--- run.py
+++ run.py
@@ -6,7 +6,10 @@ Created on Fri Dec 13 16:21:09 2013
 
 @author: Alexander Bikadorov
 
-TMalign AND blastp need to be in PATH!
+Dependencies:
+- TMalign needs to be in PATH
+- blastp needs to be in PATH
+- biopython must be installed
 
 """
 
@@ -15,10 +18,16 @@ import logging
 import argparse
 import os
 import subprocess
+import itertools
+import Bio.PDB
 
 import _myutils
 
-NUM_MODELS = 20 # number of used models per decoy
+NUM_STRUC_MODELS = 20 # number of used models per decoy
+NUM_CONSTR_MODELS = 3 # number of models used for constraint extraction per iteration
+WEIGHT_STRUC = 1
+WEIGHT_SEQ = 0.01
+D = 1 # lower bound distance in angstrom for constraint extraction
 
 def _arguments():
     parser = argparse.ArgumentParser()
@@ -40,7 +49,7 @@ def _read_scorefile(target_base_dir):
     field_dict_list = [dict(zip(field_list[0], l)) for l in field_list[1:]]
     
     # sort list
-    field_dict_list.sort(key=lambda d: float(d["score"])) 
+    field_dict_list.sort(key=lambda d: float(d["score"])) # smaller score better
     
     print('scores:')
     print('score gdtmm_full description')
@@ -50,10 +59,12 @@ def _read_scorefile(target_base_dir):
     return field_dict_list
 
 def _run_tmalign(target_pdb, template_pdb):
+    # TODO: not sure if the correct score is extracted, '-a' maybe wrong too
     args = ["TMalign "+target_pdb+" "+template_pdb+" -a | grep ^TM-score "
             """| tail -n 1 | sed -n 's|^.*TM-score= \([0-9.]\+\).*|\\1|p'"""]
-    out = subprocess.check_output(args, shell=True).rstrip().decode("utf-8")
-    return os.path.basename(template_pdb), out
+    #score = subprocess.check_output(args, shell=True).rstrip().decode("utf-8")
+    score = 42
+    return os.path.basename(template_pdb), float(score)
 
 def _tmalign(model_pdb_file, pdb_dir):
     print('running TMAlign for '+model_pdb_file+'...')    
@@ -65,7 +76,7 @@ def _tmalign(model_pdb_file, pdb_dir):
         scores += [_run_tmalign(model_pdb_file, template) for template in chunk]
         print("%d/%d" % (len(scores), len(files)))
     
-    scores.sort(key=lambda t: t[1], reverse=True)
+    scores.sort(key=lambda t: t[1], reverse=True) # higher score better
     return scores
 
 #BLAST_OUTP = '10 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore qseq sseq'
@@ -75,12 +86,38 @@ def _run_blast(target_fasta_file, model_fasta_file):
     out = subprocess.check_output(args).strip().decode("utf-8")
     return float(out.split(',')[0]) if len(out) else 0
 
+def _residue_indices(res_list, bound_list):
+    it = itertools.count(1)
+    return [a for a, x in zip((next(it) if a != '-' else None for a in res_list), bound_list) if x == ':']
+    
+def _run_tmalign_constr(target_pdb_file, template_pdb_file, d):
+    """Run TMalign to get distance constraints between atom pairs.
+       A list of tuples is returned containing the indices for amino acid pairs.
+       d: minimum distance in angstrom.
+    """
+    args = ["TMalign "+target_pdb_file+" "+template_pdb_file+" -d "+str(d)+" | tail -n 4"]
+    outp_lines = subprocess.check_output(args, shell=True).rstrip().decode("utf-8").splitlines()
+    return list(zip(_residue_indices(outp_lines[0], outp_lines[1]), _residue_indices(outp_lines[2], outp_lines[1])))
+
+def _tmalign_constr(target_pdb_file, template_pdb_file):
+    res_pairs = _run_tmalign_constr(target_pdb_file, template_pdb_file, D)
+    print(len(res_pairs), res_pairs)
+    parser = Bio.PDB.PDBParser(PERMISSIVE=1)
+    structure = parser.get_structure('id', target_pdb_file)
+    print(structure.header)
+    print(list(structure.get_residues()))
+    # TODO ... 
+
+    # AtomPair: Atom1_Name Atom1_ResNum Atom2_Name Atom2_ResNum Func_Type Func_Def
+    # AtomPair SG 5 V1 32 HARMONIC 0.0 0.2
+
 def main(argv=sys.argv):
     logging.getLogger().setLevel(logging.INFO)
     
     args = _arguments()
     target_pdb_path = os.path.abspath(args.target_pdb_file)
     target_base_dir = os.path.join(*_myutils.split_path(target_pdb_path)[:-2])
+    pdb_dir = os.path.abspath(args.pdb_dir)
     fasta_dir = os.path.abspath(args.fasta_dir)
     
     # step 1: run compbio_app
@@ -95,26 +132,33 @@ def main(argv=sys.argv):
     print('decoy selected: '+decoy_dict['description'])
     
     decoy_pdb_file = os.path.join(target_base_dir, 'models', decoy_dict['description'] + '.pdb')
-    struc_scores = _tmalign(decoy_pdb_file, args.pdb_dir)
+    struc_scores = _tmalign(decoy_pdb_file, pdb_dir)
 
     output_file_str = os.path.splitext(decoy_pdb_file)[0]+'_tmalign_scores.txt'
-    _myutils.write_file(output_file_str , '\n'.join(' '.join(t) for t in struc_scores))    
+    _myutils.write_file(output_file_str , '\n'.join(' '.join(str(t)) for t in struc_scores))    
 
-    struc_scores = struc_scores[:NUM_MODELS]
+    struc_scores = struc_scores[:NUM_STRUC_MODELS]
     
     # step 4: get sequence scores
+    # TODO: between target and model, or between decoy and model?
     target_fasta_file = os.path.splitext(target_pdb_path)[0]+'.fasta'
     scores = []
     for model_pdb_file, struc_score in struc_scores:
         model_fasta_file = os.path.join(fasta_dir, model_pdb_file.rstrip('.pdb') + '.fasta')
         seq_score = _run_blast(target_fasta_file, model_fasta_file)
-        scores.append((model_pdb_file, struc_score, seq_score))
+        scores.append((model_pdb_file, struc_score, seq_score, WEIGHT_STRUC * struc_score + WEIGHT_SEQ * seq_score))
     
+    scores.sort(key=lambda t: t[3], reverse=True)
     print('scores :')
     print('\n'.join(str(t) for t in scores))    
+
+    # step 5: get constraints from selected models    
+    constr_models = scores[:1] # TODO
+    for model_file, strs, seqs, fs in constr_models:
+        model_pdb_path = os.path.join(pdb_dir, model_file)
+        _tmalign_constr(target_pdb_path, model_pdb_path)
     
     print("DONE!")
 
 if __name__ == "__main__":
     sys.exit(main())
-
